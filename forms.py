77d761c9781be44b2ab36e3d8e4b2b6214f89381
btwiamsujal <sujalfaganiya2005@gmail.com>
2025-04-09 11:16:29 +0530

    modified forms

--- forms.py
+++ forms.py
@@ -1,3 +1,5 @@
+from forms import RegisterForm, LoginForm
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
