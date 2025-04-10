55e57167fe3548baa6ce71bff4068d15256a68d7
btwiamsujal <sujalfaganiya2005@gmail.com>
2025-04-10 08:23:16 +0530

    V1 last com

--- forms.py
+++ forms.py
@@ -7,18 +7,22 @@ class RegistrationForm(FlaskForm):
         DataRequired(message="Username is required."),
         Length(min=2, max=20, message="Username must be between 2 and 20 characters.")
     ])
+    
     email = StringField('Email', validators=[
         DataRequired(message="Email is required."),
         Email(message="Please enter a valid email address.")
     ])
+    
     password = PasswordField('Password', validators=[
         DataRequired(message="Password is required."),
         Length(min=6, message="Password must be at least 6 characters long.")
     ])
+    
     confirm_password = PasswordField('Confirm Password', validators=[
         DataRequired(message="Please confirm your password."),
         EqualTo('password', message="Passwords must match.")
     ])
+    
     submit = SubmitField('Sign Up')
 
 
@@ -27,7 +31,9 @@ class LoginForm(FlaskForm):
         DataRequired(message="Email is required."),
         Email(message="Enter a valid email.")
     ])
+    
     password = PasswordField('Password', validators=[
         DataRequired(message="Password is required.")
     ])
+    
     submit = SubmitField('Login')
